0680614c7c65d42d3d80a9a3f920e28479b4064c
CeleasaPanda <celeasa.panda@gmail.com>
2026-03-16 16:40:42 +0530

    Add Notebook in Python Source Format to Github

---
+++ first_GIT_test_connection.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+print("Testing Databricks Git integration")
+
+# COMMAND ----------
+
+print("Working on feature branch")
+print("Change made in Github to pull in Databricks")
+print("This change was made directly in GitHub MAIN branch")
+print("Change made in Github to pull in Databricks")
+
+
+# COMMAND ----------
+
+print("This line was added from Databricks conflict branch")
+
+# COMMAND ----------
+
+print("Temporary change for revert demonstration")
+print("PR revert demo change")
